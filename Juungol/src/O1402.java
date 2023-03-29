a201119388d187842e7b1560c4ecb38a0e5c13ba
cjstmdgusqw <cjstmdgusqw@gmail.com>
2023-03-29 17:33:50 +0900

    정보올림피아드 약수 구하기 1402

---
+++ Juungol/src/O1402.java
@@ -0,0 +1,28 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class O1402 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(), K = sc.nextInt() ,cnt = 0;
+		for (int i = 1; i <= N; i++) {
+			if(N % i == 0) {
+				cnt++;
+			}
+		}
+		int arr[] = new int[cnt], cnt2 = 0;
+		for (int i = 1; i <= N; i++) {
+			if(N % i == 0) {
+				arr[cnt2] = i; cnt2++;
+			}
+		}
+		
+		if(K < arr.length) {
+			System.out.println(arr[K-1]);
+		}else {
+			System.out.println(0);
+		}
+	}
+
+}
